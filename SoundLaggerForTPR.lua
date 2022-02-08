6a17f3c27adf851b12456f449369740efb25f8d0
_ <96882634+IlIlIIII@users.noreply.github.com>
2022-02-08 16:03:50 +0000

    Update SoundLaggerForTPR.lua

--- SoundLaggerForTPR.lua
+++ SoundLaggerForTPR.lua
@@ -1,1 +1,19 @@
-if game.PlaceId ~= 6675127933 then return end local sounds = { } local recurse function recurse(instance) if instance:IsA "Sound" then sounds[#sounds + 1] = instance end for _, child in ipairs (instance:GetChildren()) do recurse(child) end end recurse(game) game.DescendantAdded:connect(recurse) spawn(function() while true do game:GetService "RunService".RenderStepped:wait() for _, sound in ipairs (sounds) do sound:Play() end end end)
+if game.PlaceId ~= 6675127933 then return end
+local sounds = { }
+local recurse
+function recurse(instance)
+	if instance:IsA "Sound" then
+		sounds[#sounds + 1] = instance
+	end 
+	for _, child in ipairs (instance:GetChildren()) do
+		recurse(child) 
+	end 
+end 
+recurse(game) 
+game.DescendantAdded:connect(recurse)
+spawn(function()
+	game:GetService "RunService".RenderStepped:wait() 
+	for _, sound in ipairs (sounds) do 
+		sound:Play() 
+	end
+end)
